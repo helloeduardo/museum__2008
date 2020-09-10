2141bdd6ee6c66ae99d1f826b23d1de5234f69e8
Eduardo Parra <helloeduardo@icloud.com>
2020-09-10 09:20:31 -0600

    Add name test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -10,4 +10,11 @@ class MuseumTest < Minitest::Test
 
     assert_instance_of Museum, dmns
   end
+
+  def test_it_has_a_name
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_equal "Denver Museum of Nature and Science", dmns.name
+  end
+
 end
