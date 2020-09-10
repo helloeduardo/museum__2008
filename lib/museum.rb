f1caa85853750670b607809c6bd49465cfb3154e
Eduardo Parra <helloeduardo@icloud.com>
2020-09-10 09:33:19 -0600

    Add recommend exhibits method

--- lib/museum.rb
+++ lib/museum.rb
@@ -10,4 +10,10 @@ class Museum
     @exhibits << exhibit
   end
 
+  def recommend_exhibits(patron)
+    exhibits.find_all do |exhibit|
+      patron.interests.include?(exhibit.name)
+    end
+  end
+
 end
